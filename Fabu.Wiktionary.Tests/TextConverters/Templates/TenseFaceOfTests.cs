3377a7c6a20cea4f1e97d8e7616602d78b9b9004
Shavkat Aynurin <sh@ayn.su>
2018-05-01 11:58:48 -0700

    Some of face-tense templates

---
+++ Fabu.Wiktionary.Tests/TextConverters/Templates/TenseFaceOfTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace Fabu.Wiktionary.Tests.TextConverters.Templates
+{
+    public class TenseFaceOfTests : TestConverterFactory
+    {
+        [Fact]
+        public void ThirdPersonSingularOf()
+        {
+            var creole = "{{en-third-person singular of|pie}}";
+            var html = "<p><em>Third-person singular simple present indicative of</em> pie.</p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void ArchaicThirdPersonSingularOf()
+        {
+            var creole = "{{en-archaic third-person singular of|have}}";
+            var html = "<p><em>(archaic) Third-person singular simple present indicative of</em> have.</p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void PastOf()
+        {
+            var creole = "{{en-past of|lionize}}";
+            var html = "<p><em>Simple past and past participle of</em> lionize.</p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void SimplePastOf()
+        {
+            var creole = "{{en-simple past of|weave}}";
+            var html = "<p><em>Simple past of</em> weave.</p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+    }
+}
---
+++ Fabu.Wiktionary/TextConverters/Wikitext/Templates/TenseFaceTemplateConverters.cs
@@ -0,0 +1,44 @@
+﻿using MwParserFromScratch.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fabu.Wiktionary.TextConverters.Wiki.Templates
+{
+    class EnThirdPersonSingularOfTemplateConverter : BaseFormOfTemplatesConverter
+    {
+        protected override string[] GetPhrases() => new[]
+            {
+                "third-person singular simple present indicative of",
+                "third-person singular simple present indicative of",
+                "Third-person singular simple present indicative of"
+            };
+    }
+    class EnArchaicThirdPersonSingularOfTemplateConverter : BaseFormOfTemplatesConverter
+    {
+        protected override string[] GetPhrases() => new[]
+            {
+                "(archaic) third-person singular simple present indicative of",
+                "(archaic) third-person singular simple present indicative of",
+                "(archaic) Third-person singular simple present indicative of"
+            };
+    }
+    class EnPastOfTemplateConverter : BaseFormOfTemplatesConverter
+    {
+        protected override string[] GetPhrases() => new[]
+            {
+                "simple past and past participle of",
+                "simple past and past participle of",
+                "Simple past and past participle of"
+            };
+    }
+    class EnSimplePastOfTemplateConverter : BaseFormOfTemplatesConverter
+    {
+        protected override string[] GetPhrases() => new[]
+            {
+                "simple past of",
+                "simple past of",
+                "Simple past of"
+            };
+    }
+}
