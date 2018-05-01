6cf825a1908097d7bf9b3fd3e8a9bd474646a9fa
Shavkat Aynurin <sh@ayn.su>
2018-05-01 14:03:52 -0700

    Given Name template

---
+++ Fabu.Wiktionary.Tests/TextConverters/Templates/GivenNameTests.cs
@@ -0,0 +1,92 @@
+﻿using Xunit;
+
+namespace Fabu.Wiktionary.Tests.TextConverters.Templates
+{
+    public class GivenNameTests : TestConverterFactory
+    {
+        [Fact]
+        public void Female()
+        {
+            var creole = "{{given name|female|from=surnames}}";
+            var html = "<p><em>A female given name</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void Male()
+        {
+            var creole = "{{given name|male}}";
+            var html = "<p><em>A male given name</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void FemaleMale()
+        {
+            var creole = "{{given name|female|or=male}}";
+            var html = "<p><em>A female or male given name</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void Hebrew()
+        {
+            var creole = "{{given name|male|from=Hebrew|lang=de}}";
+            var html = "<p><em>A Hebrew male given name</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void EnglishMaleFemale()
+        {
+            var creole = "{{given name|female|or=male|from=English}}";
+            var html = "<p><em>An English female or male given name</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void EnglishNoGender()
+        {
+            var creole = "{{given name|English|A=a}}";
+            var html = "<p><em>an English given name</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void DimFemale()
+        {
+            var creole = "{{given name|female|dim=Svetlana}}";
+            var html = "<p><em>A diminutive of the female given name Svetlana</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void DimTrFemale()
+        {
+            var creole = "{{given name|female|dim=Svetlana|dimtr=Svetlanah}}";
+            var html = "<p><em>A diminutive of the female given name Svetlana (Svetlanah)</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void DimTr23Female()
+        {
+            var creole = "{{given name|female|dim=Svetlana|dim2=Svetulya|dimtr2=Svetulet|dim3=Pipa|dimalt3=Pipulya}}";
+            var html = "<p><em>A diminutive of the female given names Svetlana, Svetulya (Svetulet) or Pipulya</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void FemaleEq()
+        {
+            var creole = "{{given name|female|eq=Sweet}}";
+            var html = "<p><em>A female given name, equivalent to English Sweet</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void DimFemaleEq2()
+        {
+            var creole = "{{given name|female|dim=Svetlana|eq=Sweet|eq2=Sweeta}}";
+            var html = "<p><em>A diminutive of the female given name Svetlana, equivalent to English Sweet or Sweeta</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+        [Fact]
+        public void FemaleEq3()
+        {
+            var creole = "{{given name|female|eq3=Svetlana|eq=Sweet|eq2=Sweeta}}";
+            var html = "<p><em>A female given name, equivalent to English Sweet, Sweeta or Svetlana</em></p>";
+            Assert.Equal(html, Convert(creole).ToHtml());
+        }
+    }
+}
---
+++ Fabu.Wiktionary/TextConverters/Wikitext/Templates/GivenNameTemplateConverter.cs
@@ -0,0 +1,118 @@
+﻿using MwParserFromScratch.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fabu.Wiktionary.TextConverters.Wiki.Templates
+{
+    class GivenNameTemplateConverter : BaseTemplateConverter
+    {
+        private readonly char[] _vowels = new[] { 'a', 'e', 'i', 'o', 'u' };
+        private readonly string[] _deniedFroms = new[] { "female", "male", "surnames" };
+        protected override ConversionResult ConvertTemplate(TemplateName name, Template template, ConversionContext context)
+        {
+            var result = new ConversionResult();
+            result.Write("<em>");
+
+            template.Arguments.TryGetOneOf(out Wikitext from, "from", 1);
+            if (Array.BinarySearch(_deniedFroms, from.ToString()) >= 0)
+                from = null;
+
+            template.Arguments.TryGetArray("dim", out Wikitext[] dimValues);
+
+            var startsWithAVowel = dimValues == null && from != null && Array.BinarySearch(_vowels, Char.ToLowerInvariant(from.ToString()[0])) >= 0;
+
+            if (template.Arguments.TryGet(out Wikitext a, "A"))
+            {
+                if (a.ToString() == "a" && startsWithAVowel)
+                    result.Write("an");
+                else result.Write(a.TooSmart());
+            }
+            else
+            {
+                if (startsWithAVowel)
+                    result.Write("An");
+                else result.Write("A");
+            }
+            result.WriteSpaceIfNotEmpty();
+
+            if (dimValues != null)
+                result.Write("diminutive of the ");
+
+            if (from != null)
+                result.Write(from.TooSmart(), " ");
+
+
+            template.Arguments.TryGetOneOf(out Wikitext gender1, "gender", 1);
+            template.Arguments.TryGet(out Wikitext gender2, "or");
+
+            if (gender1 != null && Array.BinarySearch(_deniedFroms, gender1.ToString()) >= 0)
+                result.Write(gender1.TooSmart(), " ");
+            if (gender2 != null)
+                result.Write("or ", gender2.TooSmart(), " ");
+
+            if (dimValues == null || dimValues.Length <= 1)
+                result.Write("given name");
+            else
+                result.Write("given names");
+
+            if (dimValues != null)
+            {
+                for (var i = 0; i < dimValues.Length; i++)
+                {
+                    Wikitext tr = null, alt = null;
+                    if (i == 0)
+                    {
+                        template.Arguments.TryGet(out tr, "dimtr");
+                        template.Arguments.TryGet(out alt, "dimalt");
+                    }
+                    else
+                    {
+                        template.Arguments.TryGet(out tr, i + 1, "dimtr");
+                        template.Arguments.TryGet(out alt, i + 1, "dimalt");
+                    }
+
+                    if (dimValues[i] != null | alt != null)
+                    {
+                        if (i == 0)
+                            result.Write(" ");
+                        else if (i > 0)
+                        {
+                            if (i == dimValues.Length - 1)
+                                result.Write(" or ");
+                            else result.Write(", ");
+                        }
+                        if (alt != null)
+                            result.Write(alt.TooSmart());
+                        else result.Write(dimValues[i].TooSmart());
+                        if (tr != null)
+                            result.Write(" (", tr.TooSmart(), ")");
+                    }
+                }
+            }
+
+            template.Arguments.TryGetArray("eq", out Wikitext[] eqValues);
+
+            if (eqValues != null)
+            {
+                result.Write(", equivalent to English");
+                for (var i = 0; i < eqValues.Length; i++)
+                {
+                    if (i == 0)
+                        result.Write(" ");
+                    else if (i > 0)
+                    {
+                        if (i == eqValues.Length - 1)
+                            result.Write(" or ");
+                        else result.Write(", ");
+                    }
+                    result.Write(eqValues[i].TooSmart());
+                }
+            }
+
+            result.Write("</em>");
+
+            return result;
+        }
+    }
+}
